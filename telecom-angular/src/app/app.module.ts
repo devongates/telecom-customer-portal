d6cbd491dbefbcc81d289db533c16c712af07d24
Devon Gates <devonjgates@gmail.com>
2021-08-13 09:15:09 -0500

    Added device form :iphone:

--- telecom-angular/src/app/app.module.ts
+++ telecom-angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { DeviceListComponent } from './device-list/device-list.component';
@@ -11,6 +12,8 @@ import { DeviceComponent } from './device/device.component';
 import { LoginComponent } from './login/login.component';
 import { PlansComponent } from './plans/plans.component';
 import { HomeComponent } from './home/home.component';
+import { DevicePageComponent } from './device-page/device-page.component';
+import { DeviceFormComponent } from './device-form/device-form.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +24,15 @@ import { HomeComponent } from './home/home.component';
     DeviceComponent,
     LoginComponent,
     PlansComponent,
-    HomeComponent
+    HomeComponent,
+    DevicePageComponent,
+    DeviceFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
