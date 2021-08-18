a87f59e63f70171ffb07f735a29e20e244abfb48
Devon Gates <devonjgates@gmail.com>
2021-08-18 14:29:26 -0500

    Developed userplans component :hammer_and_wrench:

--- telecom-angular/src/app/models/userPlan.ts
+++ telecom-angular/src/app/models/userPlan.ts
@@ -5,11 +5,15 @@ class UserPlan {
     id: number;
     planId: number;
     userId: number;
+    plan: Plan;
+    phones: Array<Phone>
 
-    constructor(id = 0, planId = 0, userId = 0) {
+    constructor(id = 0, planId = 0, userId = 0, plan = new Plan(), phones = []) {
         this.id = id;
         this.planId = planId;
         this.userId = userId;
+        this.plan = plan;
+        this.phones = phones;
     }
 }
 
